222a086cce7bb1cce5428880db45e83eb80ad82c
Arjen Duinhouwer <administrator@Mac-arjen.local>
2013-07-06 17:16:32 +0200

    Added these files, need some work thought

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta name="keywords" content=" " />
+		
+		
+	</head>
+<body <?php body_class(); ?>>
+
+
+<header>
+
+
+	
+	
+</header>	
+		
